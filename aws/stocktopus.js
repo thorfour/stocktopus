a9aa7f1f9ee8aed86e2a51c67f4fd77fc95707ac
thor <thorvald.hansen@omnitier.com>
2016-07-21 13:08:04 -0500

    Fixed json parsing for aws

--- aws/stocktopus.js
+++ aws/stocktopus.js
@@ -2,6 +2,18 @@ var cp = require("child_process");
 
 exports.handler = function(event, context) {
 
-    var proc = cp.spawnSync("./colinmc", [event.text], {encoding : "utf8"});
-    context.succeed(proc.stdout);
+    // Parse our the request from the body
+    var queryStr = unescape(event.body)
+    var jsonStr  = '{"' + queryStr.replace(/ /g, '", "').replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
+    var ticker = JSON.parse(jsonStr).text
+
+    // Spawn the go routine to lookup stock quote
+    var proc = cp.spawnSync("./colinmc", [ticker], {stdio: 'pipe', encoding: "utf8"});
+    var quote = proc.stdout;
+
+    // Parse quote into json for slack
+    var resp = '{ "response_type" : "in_channel", "text" : "' + quote + '" }';
+
+    // Return json
+    context.succeed(resp);
 };
