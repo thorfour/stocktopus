9695b21459432083d7a815b86a3765a809ac2396
Thor <thansen@digitalocean.com>
2018-05-04 12:54:37 -0500

    Added docker build for standalone server

---
+++ cmd/server/server_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestSimpleQuote(t *testing.T) {
+
+	// Run a debug server
+	go run(8088, true)
+
+	form := url.Values{
+		"text": {"amd"},
+	}
+
+	body := bytes.NewBufferString(form.Encode())
+	resp, err := http.Post("http://localhost:8088/v1", "application/x-www-form-urlencoded", body)
+	if err != nil {
+		t.Fatalf("post request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed to read resp body: %v", err)
+	}
+
+	r := new(response)
+	if err := json.Unmarshal(b, r); err != nil {
+		t.Fatalf("failed to unmarshal response: %v", err)
+	}
+
+	if r.ResponseType != inchannel {
+		t.Errorf("unexpected response type: %v", r.ResponseType)
+	}
+}
