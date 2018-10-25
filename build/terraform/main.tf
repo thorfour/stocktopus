b780e5429a46ce8f58eb1f958f74f3e1bd678fe8
thor <github@thor-hansen.com>
2018-10-24 22:29:03 -0500

    use redis address that's created. use ssh key resource

--- build/terraform/main.tf
+++ build/terraform/main.tf
@@ -6,19 +6,46 @@ variable "region" {
     default = "nyc3"
 }
 
-variable "ssh_keys" {
-    type = "list"
-    default = []
-}
 variable "redis_pw" {}
-variable "redis_addr" {}
 variable "hostname" {
     default = "beta.stocktopus.io"
+}
+variable "ssh_key_path" {
+    default = "~/.ssh/id_rsa.pub"
 }
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = "${var.do_token}"
+}
+
+resource "digitalocean_ssh_key" "default" {
+   name = "default" 
+   public_key = "${file("${var.ssh_key_path}")}"
+}
+
+# Create separate redis droplet
+resource "digitalocean_droplet" "redis" {
+    image = "ubuntu-18-04-x64"
+    name = "stocktopus-redis"
+    region = "${var.region}"
+    size = "s-1vcpu-1gb"
+    monitoring = true
+    ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
+    tags = ["stocktopus"]
+
+    provisioner "local-exec" {
+        command = "sudo apt install docker"
+    }
+
+    provisioner "local-exec" {
+        command = "docker pull redis"
+    }
+}
+
+output "stocktopus_redis_ip" {
+    description = "redis ipv4 address"
+    value = "${digitalocean_droplet.redis.ipv4_address}"
 }
 
 # Create the stocktopus main droplet
@@ -29,7 +56,7 @@ resource "digitalocean_droplet" "stocktopus" {
     size = "s-1vcpu-1gb"
     monitoring = true
     tags = ["stocktopus"]
-    ssh_keys = "${var.ssh_keys}"
+    ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
 
     provisioner "local-exec" {
         command = "sudo apt install docker"
@@ -40,25 +67,11 @@ resource "digitalocean_droplet" "stocktopus" {
     }
 
     provisioner "local-exec" {
-        command = "docker run --name stocktopus -d -p 80:80 -p 443:443 -e REDISADDR=${var.redis_addr} -e REDISPW=${var.redis_pw} -v /cert:/cert quay.io/thorfour/stocktopus:v1.3.2 /server -host ${var.hostname} -c /cert"
+        command = "docker run --name stocktopus -d -p 80:80 -p 443:443 -e REDISADDR=${digitalocean_droplet.redis.ipv4_address} -e REDISPW=${var.redis_pw} -v /cert:/cert quay.io/thorfour/stocktopus:v1.3.2 /server -host ${var.hostname} -c /cert"
     }
 }
 
-# Create separate redis droplet
-resource "digitalocean_droplet" "redis" {
-    image = "ubuntu-18-04-x64"
-    name = "stocktopus-redis"
-    region = "${var.region}"
-    size = "s-1vcpu-1gb"
-    monitoring = true
-    ssh_keys = "${var.ssh_keys}"
-    tags = ["stocktopus"]
-
-    provisioner "local-exec" {
-        command = "sudo apt install docker"
-    }
-
-    provisioner "local-exec" {
-        command = "docker pull redis"
-    }
+output "stocktopus_ip" {
+    description = "stocktopus main ipv4"
+    value = "${digitalocean_droplet.stocktopus.ipv4_address}"
 }
