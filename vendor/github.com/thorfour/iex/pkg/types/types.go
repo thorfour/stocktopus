58525b1d82f11b85a3cddff86ffa02b86f9a9be9
Thor <thansen@digitalocean.com>
2019-04-24 14:24:58 -0500

    dep ensure -update

--- vendor/github.com/thorfour/iex/pkg/types/types.go
+++ vendor/github.com/thorfour/iex/pkg/types/types.go
@@ -9,6 +9,7 @@ const (
 	APIVersion = "1.0"
 	QuoteStr   = "quote"
 	NewsStr    = "news"
+	StatsStr   = "stats"
 	ChartStr   = "chart"
 	StockStr   = "stock"
 	PriceStr   = "price"
@@ -74,4 +75,58 @@ func (i Batch) Quote(ticker string) (Quote, error) {
 	}
 
 	return q, nil
+}
+
+// Stats holds information about a companys stats
+type Stats struct {
+	CompanyName         string      `json:"companyName"`
+	Marketcap           int64       `json:"marketcap"`
+	Beta                float64     `json:"beta"`
+	Week52High          float64     `json:"week52high"`
+	Week52Low           int         `json:"week52low"`
+	Week52Change        float64     `json:"week52change"`
+	ShortInterest       int         `json:"shortInterest"`
+	ShortDate           int         `json:"shortDate"`
+	DividendRate        float64     `json:"dividendRate"`
+	DividendYield       float64     `json:"dividendYield"`
+	ExDividendDate      string      `json:"exDividendDate"`
+	LatestEPS           float64     `json:"latestEPS"`
+	LatestEPSDate       string      `json:"latestEPSDate"`
+	SharesOutstanding   int64       `json:"sharesOutstanding"`
+	Float               int64       `json:"float"`
+	ReturnOnEquity      float64     `json:"returnOnEquity"`
+	ConsensusEPS        float64     `json:"consensusEPS"`
+	NumberOfEstimates   int         `json:"numberOfEstimates"`
+	EPSSurpriseDollar   interface{} `json:"EPSSurpriseDollar"`
+	EPSSurprisePercent  float64     `json:"EPSSurprisePercent"`
+	Symbol              string      `json:"symbol"`
+	EBITDA              int         `json:"EBITDA"`
+	Revenue             int         `json:"revenue"`
+	GrossProfit         int         `json:"grossProfit"`
+	Cash                int         `json:"cash"`
+	Debt                int         `json:"debt"`
+	TtmEPS              float64     `json:"ttmEPS"`
+	RevenuePerShare     int         `json:"revenuePerShare"`
+	RevenuePerEmployee  int         `json:"revenuePerEmployee"`
+	PeRatioHigh         int         `json:"peRatioHigh"`
+	PeRatioLow          float64     `json:"peRatioLow"`
+	ReturnOnAssets      float64     `json:"returnOnAssets"`
+	ReturnOnCapital     interface{} `json:"returnOnCapital"`
+	ProfitMargin        float64     `json:"profitMargin"`
+	PriceToSales        float64     `json:"priceToSales"`
+	PriceToBook         float64     `json:"priceToBook"`
+	Day200MovingAvg     float64     `json:"day200MovingAvg"`
+	Day50MovingAvg      float64     `json:"day50MovingAvg"`
+	InstitutionPercent  float64     `json:"institutionPercent"`
+	InsiderPercent      interface{} `json:"insiderPercent"`
+	ShortRatio          interface{} `json:"shortRatio"`
+	Year5ChangePercent  float64     `json:"year5ChangePercent"`
+	Year2ChangePercent  float64     `json:"year2ChangePercent"`
+	Year1ChangePercent  float64     `json:"year1ChangePercent"`
+	YtdChangePercent    float64     `json:"ytdChangePercent"`
+	Month6ChangePercent float64     `json:"month6ChangePercent"`
+	Month3ChangePercent float64     `json:"month3ChangePercent"`
+	Month1ChangePercent float64     `json:"month1ChangePercent"`
+	Day5ChangePercent   float64     `json:"day5ChangePercent"`
+	Day30ChangePercent  float64     `json:"day30ChangePercent"`
 }
